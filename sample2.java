c3fcb0435ebdda47b9e470b0f4a9143dc97f8210
onurcansari <onurcansari61@gmail.com>
2023-11-12 17:54:36 +0300

    Show the sign of the entered number

---
+++ sample2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class sample2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a number");
+        int num = input.nextInt();
+        if (num == 0){
+            System.out.println("This number is zero");
+        }else if (num < 0){
+            System.out.println("This number is negative");
+        }else{
+            System.out.println("This number is positive");
+        }
+    }
+}
