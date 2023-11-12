eaad17792d3de52fcd1b855cd6f33a8afca7ed89
onurcansari <onurcansari61@gmail.com>
2023-11-12 17:16:51 +0300

    Print the entered name 5 times

--- sample1.java
+++ sample1.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class sample1{
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a name");
+        String name = input.nextLine();
+        int count = 0;
+        while(count <= 5){
+            System.out.println(name);
+            count++;
+        }
     }
 }
