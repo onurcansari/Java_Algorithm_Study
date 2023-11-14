c705fcb41754a465bdc68d92492dad9f45d29ff7
onurcansari <onurcansari61@gmail.com>
2023-11-14 23:10:59 +0300

    Factorial calculation

---
+++ sample5.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class sample5 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        long value = 1;
+        System.out.println("Please enter the number to find the factorial:");
+        int factorial = input.nextInt();
+        for (int i = 1; i <= factorial; i++) {
+            value *= i;
+        }
+        System.out.println("Factorial: " + value);
+    }
+}
