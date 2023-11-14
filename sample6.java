d11b613dc5b5c5b4d148683f0d95635860ccf67d
onurcansari <onurcansari61@gmail.com>
2023-11-15 00:00:48 +0300

    Yapılacak

---
+++ sample6.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class sample6 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please");
+
+    }
+}
