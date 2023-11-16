98ca687f135618ee5ef2623eccefacf9edcc29bd
onurcansari <onurcansari61@gmail.com>
2023-11-16 22:15:40 +0300

    Substituting the value of two numbers

--- sample6.java
+++ sample6.java
@@ -3,7 +3,14 @@ import java.util.Scanner;
 public class sample6 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Please Entered a Number");
+        System.out.println("Please Entered First Number");
+        int num1 = input.nextInt();
+        System.out.println("Please Entered Second Number");
+        int num2 = input.nextInt();
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+        System.out.printf("Now Your First Number: " + num1 + " Your Second Number: " + num2);
 
     }
 }
