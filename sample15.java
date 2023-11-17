dcc2f887050e2790bd2c4b1616c92010746a4318
onurcansari <onurcansari61@gmail.com>
2023-11-17 23:02:38 +0300

    Multiplication by addition

---
+++ sample15.java
@@ -0,0 +1,18 @@
+// Multiplication by addition
+
+import java.util.Scanner;
+
+public class sample15 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please Entered The Number");
+        int num = input.nextInt();
+        System.out.println("Please Entered The Number To Multiply");
+        int multiply = input.nextInt();
+        int result = 0;
+        for (int i = 0; i<multiply; i++){
+            result +=num;
+        }
+        System.out.println("Result: " + result);
+    }
+}
