ca89ea8afa63b003b83dfa32a992b221eb67a4c6
onurcansari <onurcansari61@gmail.com>
2023-11-17 20:40:56 +0300

    Find the exponent of the entered number

---
+++ sample11.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class sample11 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please Entered The Number");
+        int num = input.nextInt();
+        System.out.println("Please enter the exponent of the number");
+        int exponent = input.nextInt();
+        int result = 1;
+        for (int i = 0; i < exponent; i++) {
+            result *=num;
+
+        }System.out.println("Your Result: " + result);
+
+    }
+}
+Find 
