9a4f243f6bbd8dbaae3ce7cd26bfc68467ed49c5
onurcansari <onurcansari61@gmail.com>
2023-11-17 14:04:17 +0300

    Find the average of numbers until 5 enters

---
+++ sample8.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class sample8 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int sum = 0;
+        int i=0;
+        while (true) {
+            int num = input.nextInt();
+            if (num != 5) {
+                sum += num;
+                i++;
+            } else {
+                break;
+            }
+
+        }
+        System.out.println(sum/i);
+    }
+}
