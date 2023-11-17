206ad349de1d262ed58b133109ca866d6b3baee4
onurcansari <onurcansari61@gmail.com>
2023-11-17 14:02:35 +0300

    Write the opposite of a binary number

---
+++ sample10.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class sample10 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String binaryNum = input.next();
+        String opposite = "";
+        for (int i = 0; i < binaryNum.length(); i++) {
+            char a = binaryNum.charAt(i);
+            if (a == '1') {
+             opposite  += '0';
+            }
+            if (a == '0'){
+                opposite += '1';
+            }
+        }
+        System.out.println(opposite);
+    }
+}
