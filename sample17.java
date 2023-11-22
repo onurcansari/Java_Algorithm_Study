88ca871ba3f4a69836f10ea068febd3fdb9a86be
onurcansari <onurcansari61@gmail.com>
2023-11-22 18:01:44 +0300

    Dönem Sonu Notunuzu ve Bütünleme Durumunuzu Bulma

---
+++ sample17.java
@@ -0,0 +1,23 @@
+// Dönem Sonu Notunuzu ve Bütünleme Durumunuzu Bulma
+
+import java.util.Scanner;
+
+public class sample17 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Lütfen Vize Notunuzu Giriniz");
+        int vize = input.nextInt();
+        System.out.println("Lütfen Final Notunuzu Giriniz");
+        int Final = input.nextInt();
+
+        double not =(vize * 0.4) + (Final * 0.6);
+
+        System.out.println("Notunuz: " + not);
+        if (not < 50){
+            System.out.println("Kaldınız Geçmiş Olsun");
+        }else if (not >= 50){
+            System.out.println("Tebrikler Geçtiniz");
+        }
+
+    }
+}
