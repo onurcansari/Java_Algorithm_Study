dc0e9fdc76ec41eed03441ce7ca1321b1f7130ee
onurcansari <onurcansari61@gmail.com>
2023-11-26 11:49:54 +0300

    Find the largest of the 10 numbers entered

---
+++ sample18.java
@@ -0,0 +1,16 @@
+// Find the largest of the 10 numbers entered
+import java.util.Scanner;
+
+public class sample18 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int large = 0;
+        for (int i = 0; i < 10; i++) {
+            int num = input.nextInt();
+            if (num % 2 != 0 && large < num) {
+                large = num;
+            }
+        }
+        System.out.println("Largest Integer: " + large);
+    }
+}
