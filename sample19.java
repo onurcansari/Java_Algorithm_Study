1c11b2e025561fc5ed91f03e2a5d5b007f41a97d
onurcansari <onurcansari61@gmail.com>
2023-12-02 22:38:57 +0300

    Find the factorial of nums up to an entered num

---
+++ sample19.java
@@ -0,0 +1,20 @@
+// Calculate the factorial of numbers up to the entered number
+import java.util.Scanner;
+
+public class sample19 {
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+        System.out.println("Please Entered A Number");
+        int num = input.nextInt();
+        int result = 1;
+        for (int i = 0; i <=num; i++){
+            for (int j = 1; j <=i; j++){
+                result *= j;
+               
+            } 
+            System.out.println(i+"! : " + result);
+            result = 1;
+        } 
+    }
+    
+}
